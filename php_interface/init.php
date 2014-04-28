cc0d63df35947343c69534d81cdbdc4307e71110
Alexandr Borisov <sasha@ubuntuSasha>
2014-04-28 17:59:38 +0300

    добавлены константы смс логина

--- php_interface/init.php
+++ php_interface/init.php
@@ -2,6 +2,8 @@
 error_reporting(E_ALL);
 session_start(0);
 define("VERSION", 109);
+define("SMS_LOGIN", "380673666811");
+define("SMS_PASSW", "swimmer")
 date_default_timezone_set('Europe/Kiev');
 require_once($_SERVER['DOCUMENT_ROOT'].'/lib/main_lib_inc.php');
 require_once('db_init_inc.php');
