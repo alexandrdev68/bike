b70ee3419d443e3c78b13d469d485738de64f8ad
Alexandr Borisov <sasha@ubuntuSasha>
2014-07-11 12:26:58 +0300

    changed version

--- php_interface/init.php
+++ php_interface/init.php
@@ -1,7 +1,7 @@
 <?ini_set('display_errors', 1);
 error_reporting(E_ERROR);
 session_start(0);
-define("VERSION", 136);
+define("VERSION", 137);
 define("SMS_LOGIN", "380673666811");
 define("SMS_PASSW", "swimmer");
 define("SMS_API_KEY", "4f1c76b4ae78754d1bfa5cbaaad31522e99a1d63");
