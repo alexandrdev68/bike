0583e6841ddf5958e7aa43517b823f738780de1c
Alexandr Borisov <sasha@ubuntuSasha>
2016-03-05 17:30:24 +0200

    Верстка форми оплати та авторизації - реєстрації

--- components/public/order_payment/template/template.php
+++ components/public/order_payment/template/template.php
@@ -7,11 +7,49 @@
         <h4 class="modal-title"><?=TEMP::$Lang['payment_window_header_text']?></h4>
       </div>
       <div class="modal-body">
-        <p>One fine body&hellip;</p>
+		<div class="row">
+			<div class="col-md-5">
+			  <div class="">
+			    <div class="thumbnail">
+			      <img src="..." alt="...">
+			      <div class="caption">
+			        <h3>Thumbnail label</h3>
+			        <p>...</p>
+			        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+			      </div>
+			    </div>
+			  </div>
+		  </div>
+		
+		<div class="col-md-7">
+		<form class="form-horizontal">
+		  <div class="form-group">
+		    <div class="col-xs-4">
+			    <label for="InputPhone"><?=TEMP::$Lang['input_phone'].' ('.TEMP::$Lang['text_phone_template'].')'?></label>
+			    <input type="text" name="phone" class="form-control" id="InputPhone" placeholder="<?=TEMP::$Lang['input_phone']?>">
+		    </div>
+		  </div>
+		  <div class="form-group">
+		    <div class="col-xs-5">
+			    <label for="InputLastname"><?=TEMP::$Lang['input_lastName']?></label>
+			    <input type="email" name="lastname" class="form-control" id="InputLastname" placeholder="<?=TEMP::$Lang['input_lastName']?>">
+		    </div>
+		    <div class="col-xs-5">
+			    <label for="InputFirsttname"><?=TEMP::$Lang['input_firstName']?></label>
+			    <input type="email" name="firstname" class="form-control" id="InputFirstname" placeholder="<?=TEMP::$Lang['input_firstName']?>">
+		    </div>
+		    <div class="col-xs-5">
+			    <label for="InputSecondtname"><?=TEMP::$Lang['input_patronymic']?></label>
+			    <input type="email" name="secondname" class="form-control" id="InputSecondname" placeholder="<?=TEMP::$Lang['input_patronymic']?>">
+		    </div>
+		  </div>
+		  <button type="submit" class="btn btn-default"><?=TEMP::$Lang['sms_text_4']?></button>
+		</form>
+		</div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal"><?=TEMP::$Lang['exit_btn']?></button>
-        <button data-vtemplate_payment_window="value=bike_number,event=click:payment_okhandler" type="button" class="btn btn-danger"><?=TEMP::$Lang['pay_booking_text']?></button>
+        <button data-vtemplate_payment_window="value=bike_number,event=click:payment_okhandler" disabled type="button" class="btn btn-danger"><?=TEMP::$Lang['pay_booking_text']?></button>
       </div>
     </div><!-- /.modal-content -->
  
